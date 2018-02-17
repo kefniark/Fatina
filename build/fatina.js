3d84fe8a7e7558ac6bf681d3c53cfa3e9fd43734
Kevin Destrem <kevin@destrem.net>
2018-02-17 14:21:13 +0900

    Update dependencies

--- build/fatina.js
+++ build/fatina.js
@@ -7,7 +7,7 @@
 		exports["Fatina"] = factory();
 	else
 		root["Fatina"] = factory();
-})(this, function() {
+})(typeof self !== 'undefined' ? self : this, function() {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
